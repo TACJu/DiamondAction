6e777eed474a96d029e16b6b153c778fc5ae93d3
TACJu <heju@pku.edu.cn>
2019-03-19 19:17:52 +0800

    fix tiny bug

---
+++ pages/diamonds/diamonds.js
@@ -0,0 +1,37 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 获取商品信息，callback用于指定是否调用wx.stopPullDownRefresh()
+   */
+  getGoods(callback) {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
