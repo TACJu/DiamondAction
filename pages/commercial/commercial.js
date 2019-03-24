12c5086b5ca71a7bbed5b52cc38cef3d04805dec
TACJu <heju@pku.edu.cn>
2019-03-24 14:04:20 +0800

    change image

--- pages/commercial/commercial.js
+++ pages/commercial/commercial.js
@@ -31,6 +31,15 @@ Page({
     this.setData({
       merchandise: localData.goodsList
     })
+    var len = this.data.merchandise.length;
+    for (var i = 0; i < len; i++)
+    {
+      this.data.merchandise[i]['imageID'] = this.data.merchandise[i]['id'] % 5;
+      console.log(this.data.merchandise[i]['imageID'])
+    }
+    this.setData({
+      merchandise: this.data.merchandise
+    })
   },
 
   goToSubpage(event)
