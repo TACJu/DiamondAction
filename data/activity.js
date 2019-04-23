18f9c57e9315b1e1d3dfb82e913d913d4e9b3e44
TACJu <heju@pku.edu.cn>
2019-04-23 18:51:35 +0800

    add free

--- data/activity.js
+++ data/activity.js
@@ -2,11 +2,17 @@ var activityJson = [
   {
     "id": 0,
     "photopath": '/images/Activity/',
+    "name": 'free',
+    "description": '谢欣彤于2019年2月28日软工课上挖出了这颗钻石'
+  },
+  {
+    "id": 1,
+    "photopath": '/images/Activity/',
     "name": 'xiaoqing',
     "description": '武家伟于2019年2月28日软工课上挖出了这颗钻石'
   },
   {
-    "id": 1,
+    "id": 2,
     "photopath": '/images/Activity/',
     "name": 'libai',
     "description": '谷典典于2019年2月28日软工课上挖出了这颗钻石'
@@ -16,7 +22,7 @@ var activityJson = [
     "photopath": '/images/Activity/',
     "name": 'liulangdiqiu',
     "description": '谢欣彤于2019年2月28日软工课上挖出了这颗钻石'
-  }
+  },
 ]
 
 module.exports = {
