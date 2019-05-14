7cbbb4b99485fff673961bc9fb5ad4adc34158e9
TACJu <heju@pku.edu.cn>
2019-05-14 20:38:01 +0800

    dig done

--- pages/dig/dig.js
+++ pages/dig/dig.js
@@ -1,4 +1,5 @@
-// pages/dig/dig.js
+var util = require('../../utils/util.js')
+
 Page({
 
   /**
@@ -8,41 +9,57 @@ Page({
     query: '',
     select: '',
     items: [
-      { name: 'USA', value: '美国', checked: false},
-      { name: 'CHN', value: '中国', checked: true },
-      { name: 'BRA', value: '巴西', checked: false },
-      { name: 'JPN', value: '日本', checked: false },
-      { name: 'ENG', value: '英国', checked: false },
-      { name: 'TUR', value: '法国', checked: false },
+      { name: 'provision', value: '我已阅读并同意上述条款', checked: false},
     ]
   },
 
-  valueChange:function(e) {
-    var items = this.data.items;
-    for (var i = 0; i < items.length; i++) {
-      if (items[i].name == this.data.select) {
-        for (var j = 0; j < items.length; j++) {
-          // console.log("items[j].checked = ", items[j].checked);
-          if (items[j].checked && j != i) {
-            items[j].checked = false;
-          }
-        }
-        items[i].checked = !(items[i].checked);
-        console.log("-----:", items);
-        // this.setData(this.data.items[i]);
-
-      }
-    }
+  checkChange(e) {
+    var items = this.data.items
+    items[0].checked = !(items[0].checked)
     this.setData({
-      items: items
-    });
-  },
-
-  radioChange(e) {
-    this.setData({
+      items: items,
       select: e.detail.value
     })
     console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+
+  goToDig() {
+    var _this = this
+    var time = util.formatTime(new Date())
+    var privKey = "YmYyYjc3MWZmNzExMGFlMmE4NDhlYzVlN2YxYTZjNWQ0MDM4YjVhMTcyMmI4NWJhMjU1MWM2NTQ3Y2U4ZGYyNmFiNzdlZjQ1ZGU1ZjJiNGE2NWNkZjhmN2UyMzJiODg4OGYwMDBjYWZhMDkyOGU3ZWZhMWI4NDU5NDg3ZDQxMDk0ODIyNjk2ZGMyNzI2ODFiMmRiMTA3YzMxYzBlMDA2N2NkNDFjOWM3NDg1MGZlNjcyZDYxMzA0Y2M0MGQzNTJlNzMwMGU2YjJiMmJhZGY5OGJkZWEyZjEzNmI0YTc2MWE4NDE0NDBlMWViZGYwNGQyYWJhMzE5Y2RkNjEzOGM2ZmRiNDdlMDc5NzdmYTIxZDc5Y2ZiZDk0MGQxOWNlNjAzNjM1ZGQ2YmI4ZGVlZDkxZjk4NzE3YTI4NGVkMzQyMTA4Yzg2MjgwMmZiMTZiMTUyYjUwYjliNWU2NzM4YzhjMDYyMDJmZDI3NmUwOWI0MTFjMDc2NTBjZGI2ZGMxYWFkNDQ4MTg5N2IxMWY5MTU4MGI3NzY1OWIxYzczOWU4ZGQ2ZWNhNDViMGNkY2Q2MGRhZWI1OWNiNjM3NzEzOGNlMWY4YTA3NzI0NmIyMWJjNjIzYTNmNGMzYTg2OTNlMDE5NjA5YzUxYWI3YTdhYTdlZmRkYTAxY2Q4ZDdhMWM5ODksMTAwMDEsYjdhYzU0NDUzMzA1N2RkNmVkZTczYWFiNDQyNWE4MGUyNDYxMTgyOWE4ZGYyNjFhMDYzNzNlN2RmODkxNjlhZGU2YmI1MWZhNjg0MDNhMDBiZTM1OWFjYWQ3ZGFmMWFmY2FlZDNhNDMxN2RkMTdlOWU5ZjViYzQwNjg0NDY3ZDMzYTJhNGRlZjc2MGVhM2Y1ZDBmZGIxM2U4NWRhZjIwYWM5OGMzNzA5MzA3MzE5MmIxYWNmYjEyZjYwODI2YzIxNTdiZjdhZTgyMDhmMjliYmMwNjQ5YjljNjVjZTYyMTBmMWViZGZiNmYxMDAxMmNiMzJiYjdmNTQ2NDI1ZGUxYzA0NzQ2NzM2ZWU4MzhkYzU0YTc2ZTQ2ZWE1M2MyZTJkNGZmNWZmN2M1YWJiMWJmOGYzMTVlZjk5OGZkOGQ5YzMzMDFhYzhhYjk4MDMxMzgyMThjNGY3MDUyMWJmZDg4ZDk0OTcxMWEzYTc3OWIwZGYxNmRkZTI4Y2NlYWI5YWMwZmQyZjdkYWEwZDUzYzhjMThiZjk0NmEzNjljZjk2MmM1YzhmYzhlMWQ3MDU3OGU4N2Q2MDNlYzlhYjYwZTFiYTg1Nzg3Zjk5MzU2ZGEwMDVkZGVmNjQxMzU0Mjg2NGNiODhhMzE3OTI0YWZkMjNkZDdmOWYzYmQwYTgwNDMxNzE=";
+    if (_this.data.items[0].checked == false)
+    {
+      wx.showToast({
+        title: '请先同意挖掘条款',
+        icon: 'none',
+        // image: '../../images/icon/fail.jpg',
+        duration: 2000
+      })
+    }
+    else
+    {
+      wx.request({
+        url: 'https://diamondaction.internetapi.cn:8443/SCIDE/SCManager',
+        data: {
+          "action": "executeContract",
+          "contractID": "DiamondOperation",
+          "operation": "mining",
+          "arg": encodeURIComponent("{\"user\": 1, \"content\":\"" + _this.data.query + "\", \"time\":\"" + time + "\"}"),
+          "privKey": privKey
+        },
+        dataType: "jsonp",
+        success: function (res) {
+          if (res.data) {
+            console.log(res.data)
+            wx.showToast({
+              title: '成功，请耐心等待审核',
+              icon: 'none',
+              duration: 2000
+            })
+          }
+        }
+      })
+    }
   },
 
   /**
