dea379e259bbefba22c25f2f8ad44b69e7e3a4f2
via12 <785725481@qq.com>
2019-05-15 14:04:31 +0800

    share

--- pages/diamonds/diamonds.js
+++ pages/diamonds/diamonds.js
@@ -4,15 +4,42 @@ Page({
    * 页面的初始数据
    */
   data: {
+    classid:"",
+    innerid: "",
+    time: "",
+    description: "",
+    content:"",
+    price: "",
+
+
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {
 
+  onLoad: function (options) {
+    /*
+    myurl='/pages/diamonds/diamonds?classid=' +classid
+    myurl+='&innerid='+innerid
+    myurl+='&time='+time
+    myurl+='&description'+description
+    */
+    this.setData({
+      classid: options.classid,
+      innerid: options.innerid,
+      time: options.time,
+      description: options.description,
+      content:options.content,
+      price:options.price,
+
+    });
+    console.log("diamondinfo")
+    console.log(options)
+    //console.log(diamondinfo)
   },
+
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
@@ -30,13 +57,16 @@ Page({
 
   hello(event){
     let that = this;
+    let titlename = event.currentTarget.dataset.content;
+    console.log(event);
+    console.log(event.currentTarget.dataset);
+    console.log(event.currentTarget.dataset.content);
     return {
-      title: '两开花，两开花', // 转发后 所显示的title
-      path: '/pages/group/index', // 相对的路径
+      title: titlename, // 转发后 所显示的title
+      path: '/pages/diamons/diamonds', // 相对的路径
       success: (res) => {    // 成功后要做的事情
         console.log(res.shareTickets[0])
         // console.log
-
         wx.getShareInfo({
           shareTicket: res.shareTickets[0],
           success: (res) => {
@@ -60,11 +90,20 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function (options) {
     let that = this;
+
+    //let titlename = options.target.dataset.content;
+    //console.log("share")
+    //console.log(options)
+    //console.log(titlename)
+    let titlename="快来看我的独有钻链钻石"
+
+    
+
     return {
-      title: '两开花，两开花', // 转发后 所显示的title
-      path: '/pages/group/index', // 相对的路径
+      title: titlename, // 转发后 所显示的title
+      path: '/pages/diamons/diamonds', // 相对的路径
       success: (res) => {    // 成功后要做的事情
         console.log(res.shareTickets[0])
         // console.log
