f2c50414f56099bcef7cb5c76e3d456d4b17770b
TACJu <heju@pku.edu.cn>
2019-05-15 08:41:34 +0800

    fix bug

--- pages/subcommercial/subcommercial.js
+++ pages/subcommercial/subcommercial.js
@@ -13,7 +13,7 @@ Page({
     id:0,
     tmpcnt:2,
     price:0,
-    clickable:false,
+    banable:false,
     merchandise: {
       "id": 0,
       "photopath": 'https://6d61-master-27262a-1259058618.tcb.qcloud.la/images/Diamond/%E6%9C%89%E9%92%B1-%E9%81%93%E8%B7%AF-%E8%B4%AB%E7%A9%B7_02.jpg?sign=84175aa5a9fe22746bcd36618efaa12b&t=1557232644',
@@ -46,7 +46,7 @@ Page({
   goToBuy() {
     var _this = this
     _this.setData({
-      clickable: true
+      banable: true
     })
     var time = util.formatTime(new Date())
     var privKey = "YmYyYjc3MWZmNzExMGFlMmE4NDhlYzVlN2YxYTZjNWQ0MDM4YjVhMTcyMmI4NWJhMjU1MWM2NTQ3Y2U4ZGYyNmFiNzdlZjQ1ZGU1ZjJiNGE2NWNkZjhmN2UyMzJiODg4OGYwMDBjYWZhMDkyOGU3ZWZhMWI4NDU5NDg3ZDQxMDk0ODIyNjk2ZGMyNzI2ODFiMmRiMTA3YzMxYzBlMDA2N2NkNDFjOWM3NDg1MGZlNjcyZDYxMzA0Y2M0MGQzNTJlNzMwMGU2YjJiMmJhZGY5OGJkZWEyZjEzNmI0YTc2MWE4NDE0NDBlMWViZGYwNGQyYWJhMzE5Y2RkNjEzOGM2ZmRiNDdlMDc5NzdmYTIxZDc5Y2ZiZDk0MGQxOWNlNjAzNjM1ZGQ2YmI4ZGVlZDkxZjk4NzE3YTI4NGVkMzQyMTA4Yzg2MjgwMmZiMTZiMTUyYjUwYjliNWU2NzM4YzhjMDYyMDJmZDI3NmUwOWI0MTFjMDc2NTBjZGI2ZGMxYWFkNDQ4MTg5N2IxMWY5MTU4MGI3NzY1OWIxYzczOWU4ZGQ2ZWNhNDViMGNkY2Q2MGRhZWI1OWNiNjM3NzEzOGNlMWY4YTA3NzI0NmIyMWJjNjIzYTNmNGMzYTg2OTNlMDE5NjA5YzUxYWI3YTdhYTdlZmRkYTAxY2Q4ZDdhMWM5ODksMTAwMDEsYjdhYzU0NDUzMzA1N2RkNmVkZTczYWFiNDQyNWE4MGUyNDYxMTgyOWE4ZGYyNjFhMDYzNzNlN2RmODkxNjlhZGU2YmI1MWZhNjg0MDNhMDBiZTM1OWFjYWQ3ZGFmMWFmY2FlZDNhNDMxN2RkMTdlOWU5ZjViYzQwNjg0NDY3ZDMzYTJhNGRlZjc2MGVhM2Y1ZDBmZGIxM2U4NWRhZjIwYWM5OGMzNzA5MzA3MzE5MmIxYWNmYjEyZjYwODI2YzIxNTdiZjdhZTgyMDhmMjliYmMwNjQ5YjljNjVjZTYyMTBmMWViZGZiNmYxMDAxMmNiMzJiYjdmNTQ2NDI1ZGUxYzA0NzQ2NzM2ZWU4MzhkYzU0YTc2ZTQ2ZWE1M2MyZTJkNGZmNWZmN2M1YWJiMWJmOGYzMTVlZjk5OGZkOGQ5YzMzMDFhYzhhYjk4MDMxMzgyMThjNGY3MDUyMWJmZDg4ZDk0OTcxMWEzYTc3OWIwZGYxNmRkZTI4Y2NlYWI5YWMwZmQyZjdkYWEwZDUzYzhjMThiZjk0NmEzNjljZjk2MmM1YzhmYzhlMWQ3MDU3OGU4N2Q2MDNlYzlhYjYwZTFiYTg1Nzg3Zjk5MzU2ZGEwMDVkZGVmNjQxMzU0Mjg2NGNiODhhMzE3OTI0YWZkMjNkZDdmOWYzYmQwYTgwNDMxNzE=";
@@ -68,9 +68,9 @@ Page({
             duration: 2000
           })
         }
+        _this.onLoad(_this.data.options)
       }
     })
-    _this.onLoad(_this.data.options)
   },
 
   updatecnt(callback) {
@@ -139,15 +139,10 @@ Page({
     console.log(app.goodsInfo)
     this.setData({
       options:options,
-      clickable:false,
+      banable:false,
       id:options.id,
       merchandise:app.goodsInfo[options.id - 1]
     });
-
-    // console.log(app.goodsInfo)
-
-    // console.log(this.data.id)
-    // console.log(this.data.merchandise)
 
     this.updatecnt()
   },
